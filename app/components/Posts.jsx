a6bcd63993b6b4e3fc5166a12449f4c6b0d128ba
accamamuhammad <140168234+accamamuhammad@users.noreply.github.com>
2024-08-17 20:52:18 +0100

    .

--- app/components/Posts.jsx
+++ app/components/Posts.jsx
@@ -9,6 +9,7 @@ const Posts = () => {
       <h1 className="text-xl mb-4 text-primaryText">Posts</h1>
       <ul className="gap-4 sm:gap-3.5 text-sm flex flex-col items-start justify-start">
         {/* Projects List */}
+
         <Link
           href="https://accamamuhammad.hashnode.dev/7-reasons-why-tailwindcss-outshines-traditional-css"
           target="_blank"
@@ -18,6 +19,18 @@ const Posts = () => {
             30 07 2024 {""}
             <span className={PostStyling}>
               7 Reasons Why TailwindCSS Outshines Traditional CSS
+            </span>
+          </li>
+        </Link>
+        <Link
+          href="https://accamamuhammad.hashnode.dev/5-projects-that-will-get-you-hired-as-a-developer?"
+          target="_blank"
+          className="text-TertiaryBg"
+        >
+          <li className="text-primaryText">
+            17 08 2024 {""}
+            <span className={PostStyling}>
+              5 Projects That Will Get You Hired as a Developer
             </span>
           </li>
         </Link>
